1838b8f47af5f42f1be6a808679f97aab8f9caff
ilyaperepelitsa <ilyaperepelitsa@gmail.com>
2015-12-12 12:01:47 -0500

    new things
    as

--- NSF.R
+++ NSF.R
@@ -1,3 +1,5 @@
+
+
 schools16 <- read.table(file="~/Dropbox/NSF/2016.csv", sep=",",header=TRUE)
 schools15 <- read.table(file="~/Dropbox/NSF/2015.csv", sep=",",header=TRUE)
 schools14 <- read.table(file="~/Dropbox/NSF/2014.csv", sep=",",header=TRUE)
@@ -19,24 +21,24 @@ schools99 <- read.table(file="~/Dropbox/NSF/1999.csv", sep=",",header=TRUE)
 
 # pew
 
-schools16<- data.frame(schools16[7],schools16[9], schools16[13], schools16[14], schools16[18], schools16[21:23], schools16[29], schools16[34], schools16[47])
-schools15<- data.frame(schools15[7],schools15[9], schools15[13], schools15[14], schools15[18], schools15[21:23], schools15[29], schools15[34], schools15[47])
-schools14<- data.frame(schools14[7],schools14[9], schools14[13], schools14[14], schools14[18], schools14[21:23], schools14[29], schools14[34], schools14[47])
-schools13<- data.frame(schools13[7],schools13[9], schools13[13], schools13[14], schools13[18], schools13[21:23], schools13[29], schools13[34], schools13[47])
-schools12<- data.frame(schools12[7],schools12[9], schools12[13], schools12[14], schools12[18], schools12[21:23], schools12[29], schools12[34], schools12[47])
-schools11<- data.frame(schools11[7],schools11[9], schools11[13], schools11[14], schools11[18], schools11[21:23], schools11[29], schools11[34], schools11[47])
-schools10<- data.frame(schools10[7],schools10[9], schools10[13], schools10[14], schools10[18], schools10[21:23], schools10[29], schools10[34], schools10[47])
-schools09<- data.frame(schools09[7],schools09[9], schools09[13], schools09[14], schools09[18], schools09[21:23], schools09[29], schools09[34], schools09[47])
-schools08<- data.frame(schools08[7],schools08[9], schools08[13], schools08[14], schools08[18], schools08[21:23], schools08[29], schools08[34], schools08[47])
-schools07<- data.frame(schools07[7],schools07[9], schools07[13], schools07[14], schools07[18], schools07[21:23], schools07[29], schools07[34], schools07[47])
-schools06<- data.frame(schools06[7],schools06[9], schools06[13], schools06[14], schools06[18], schools06[21:23], schools06[29], schools06[34], schools06[47])
-schools05<- data.frame(schools05[7],schools05[9], schools05[13], schools05[14], schools05[18], schools05[21:23], schools05[29], schools05[34], schools05[47])
-schools04<- data.frame(schools04[7],schools04[9], schools04[13], schools04[14], schools04[18], schools04[21:23], schools04[29], schools04[34], schools04[47])
-schools03<- data.frame(schools03[7],schools03[9], schools03[13], schools03[14], schools03[18], schools03[21:23], schools03[29], schools03[34], schools03[47])
-schools02<- data.frame(schools02[7],schools02[9], schools02[13], schools02[14], schools02[18], schools02[21:23], schools02[29], schools02[34], schools02[47])
-schools01<- data.frame(schools01[7],schools01[9], schools01[13], schools01[14], schools01[18], schools01[21:23], schools01[29], schools01[34], schools01[47])
-schools00<- data.frame(schools00[7],schools00[9], schools00[13], schools00[14], schools00[18], schools00[21:23], schools00[29], schools00[34], schools00[47])
-schools99<- data.frame(schools99[7],schools99[9], schools99[13], schools99[14], schools99[18], schools99[21:23], schools99[29], schools99[34], schools99[47])
+schools16<- data.frame(schools16[7],schools16[9], schools16[13], schools16[14], schools16[18], schools16[21:23], schools16[29], schools16[34], schools16[47:48], stringsAsFactors = FALSE)
+schools15<- data.frame(schools15[7],schools15[9], schools15[13], schools15[14], schools15[18], schools15[21:23], schools15[29], schools15[34], schools15[47:48], stringsAsFactors = FALSE)
+schools14<- data.frame(schools14[7],schools14[9], schools14[13], schools14[14], schools14[18], schools14[21:23], schools14[29], schools14[34], schools14[47:48], stringsAsFactors = FALSE)
+schools13<- data.frame(schools13[7],schools13[9], schools13[13], schools13[14], schools13[18], schools13[21:23], schools13[29], schools13[34], schools13[47:48], stringsAsFactors = FALSE)
+schools12<- data.frame(schools12[7],schools12[9], schools12[13], schools12[14], schools12[18], schools12[21:23], schools12[29], schools12[34], schools12[47:48], stringsAsFactors = FALSE)
+schools11<- data.frame(schools11[7],schools11[9], schools11[13], schools11[14], schools11[18], schools11[21:23], schools11[29], schools11[34], schools11[47:48], stringsAsFactors = FALSE)
+schools10<- data.frame(schools10[7],schools10[9], schools10[13], schools10[14], schools10[18], schools10[21:23], schools10[29], schools10[34], schools10[47:48], stringsAsFactors = FALSE)
+schools09<- data.frame(schools09[7],schools09[9], schools09[13], schools09[14], schools09[18], schools09[21:23], schools09[29], schools09[34], schools09[47:48], stringsAsFactors = FALSE)
+schools08<- data.frame(schools08[7],schools08[9], schools08[13], schools08[14], schools08[18], schools08[21:23], schools08[29], schools08[34], schools08[47:48], stringsAsFactors = FALSE)
+schools07<- data.frame(schools07[7],schools07[9], schools07[13], schools07[14], schools07[18], schools07[21:23], schools07[29], schools07[34], schools07[47:48], stringsAsFactors = FALSE)
+schools06<- data.frame(schools06[7],schools06[9], schools06[13], schools06[14], schools06[18], schools06[21:23], schools06[29], schools06[34], schools06[47:48], stringsAsFactors = FALSE)
+schools05<- data.frame(schools05[7],schools05[9], schools05[13], schools05[14], schools05[18], schools05[21:23], schools05[29], schools05[34], schools05[47:48], stringsAsFactors = FALSE)
+schools04<- data.frame(schools04[7],schools04[9], schools04[13], schools04[14], schools04[18], schools04[21:23], schools04[29], schools04[34], schools04[47:48], stringsAsFactors = FALSE)
+schools03<- data.frame(schools03[7],schools03[9], schools03[13], schools03[14], schools03[18], schools03[21:23], schools03[29], schools03[34], schools03[47:48], stringsAsFactors = FALSE)
+schools02<- data.frame(schools02[7],schools02[9], schools02[13], schools02[14], schools02[18], schools02[21:23], schools02[29], schools02[34], schools02[47:48], stringsAsFactors = FALSE)
+schools01<- data.frame(schools01[7],schools01[9], schools01[13], schools01[14], schools01[18], schools01[21:23], schools01[29], schools01[34], schools01[47:48], stringsAsFactors = FALSE)
+schools00<- data.frame(schools00[7],schools00[9], schools00[13], schools00[14], schools00[18], schools00[21:23], schools00[29], schools00[34], schools00[47:48], stringsAsFactors = FALSE)
+schools99<- data.frame(schools99[7],schools99[9], schools99[13], schools99[14], schools99[18], schools99[21:23], schools99[29], schools99[34], schools99[47:48], stringsAsFactors = FALSE)
 
 schoolsall <- rbind(schools99, schools00, schools01, schools02, schools03, schools04, schools05, 
                     schools06, schools07, schools08, schools09, schools10, schools11, schools12, 
@@ -52,17 +54,44 @@ schoolsall$obligation_action_date <- as.Date(schoolsall$obligation_action_date, 
 class(schoolsall$starting_date)
 
 
-### NEW ROWS/COLUMNS
-res_duration <- c(schoolsall$ending_date - schoolsall$starting_date)/30
-costmon <-  c(schoolsall$fed_funding_amount/schoolsall$res_duration)
 
 
 
 
 ###INCLUDING  ROWS/COLUMNS INTO DF
+res_duration <- c(schoolsall$ending_date - schoolsall$starting_date)/30
 schoolsall <- data.frame(schoolsall, res_duration)
+costmon <-  c(schoolsall$fed_funding_amount/schoolsall$res_duration)
 schoolsall <- data.frame(schoolsall, costmon)
 
 
 ### SAVING THE DF
 save(schoolsall, file="NSF.RData")
+save(sumcolbyyeardf, file="sumcolbyyeardf.RData")
+save(sumcolbyprogdf, file="sumcolbyprogdf.RData")
+### trying new tables
+
+
+# create a df with a total annual amount per college 
+sumcolbyyear <- aggregate(fed_funding_amount ~ recipient_name + fiscal_year, schoolsall, sum)
+sumcolbyyeardf <- dcast(sumcolbyyear, recipient_name ~ fiscal_year, value.var="fed_funding_amount") 
+
+
+# create a df with a total annual amount per program type
+
+sumcolbyprog <- aggregate(fed_funding_amount ~ cfda_program_title + fiscal_year, schoolsall, sum)
+sumcolbyprogdf <- dcast(sumcolbyprog, cfda_program_title ~ fiscal_year, value.var="fed_funding_amount") 
+
+sumcolbystate <- aggregate(fed_funding_amount ~ principal_place_state_code + fiscal_year, schoolsall, sum)
+sumcolbystatedf <- dcast(sumcolbystate, principal_place_state_code ~ fiscal_year, value.var="fed_funding_amount") 
+
+schoolsall$principal_place_state_code <- toupper(schoolsall$principal_place_state)
+
+prognunique <- unique(schoolsall$cfda_program_title)
+prognunique
+
+ggplot(schoolsall,aes(x=obligation_action_date, y = log(costmon))) + geom_point(aes(color= action_type )) 
+
+
+ch <- gconnect("ilyaperepelitsa@gmail.com", "")
+peew <- gtrends(c("NSF", "government shutdown", "Congress"))
