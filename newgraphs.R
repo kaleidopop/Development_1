7848bf68a63132f1c040e331368ac2826ef73270
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2015-12-14 17:26:34 -0500

    change things
    new graphs - legend edited

--- newgraphs.R
+++ newgraphs.R
@@ -3,7 +3,7 @@ sumcolbystate <- aggregate(fed_funding_amount ~ principal_place_state_code + fis
 ggplot(sumcolbystate, aes(x=fiscal_year, y=fed_funding_amount)) + geom_line(aes(colour = principal_place_state_code))+ facet_wrap(~principal_place_state_code) 
 
 ## state summaries by type of research
-ggplot(schoolsall, aes(x=cfda_program_title, y=log(costmon), fill = cfda_program_title)) + geom_jitter(alpha=I(1/8), aes(color=cfda_program_title)) + facet_wrap(~principal_place_state_code) 
+ggplot(schoolsall, aes(x=cfda_program_title, y=log(costmon), fill = cfda_program_title)) + geom_jitter(alpha=I(1/2), aes(color=cfda_program_title)) + facet_wrap(~principal_place_state_code) +guides(colour = guide_legend(override.aes = list(size=12)))
 bigstates <- schoolsall[which(schoolsall$principal_place_state_code == "CA" | schoolsall$principal_place_state_code == "CO" | schoolsall$principal_place_state_code == "DC" | schoolsall$principal_place_state_code == "IL"| schoolsall$principal_place_state_code == "MA"| schoolsall$principal_place_state_code == "NY"| schoolsall$principal_place_state_code == "PA"| schoolsall$principal_place_state_code == "TX"), ]
 ggplot(bigstates, aes(x=cfda_program_title, y=log(costmon), fill = cfda_program_title)) + geom_jitter(alpha=I(1/2), aes(color=cfda_program_title)) + facet_wrap(~principal_place_state_code) +guides(colour = guide_legend(override.aes = list(size=12)))
 
@@ -12,4 +12,4 @@ ggplot(schoolsall, aes(x=cfda_program_title, y=log(costmon), fill = cfda_program
 
 ### types of research - by new/cont/revise
 
-ggplot(schoolsall, aes(x=action_type, y=log(costmon), fill = action_type)) + geom_jitter(alpha=I(1/2), aes(color=action_type)) + facet_wrap(~cfda_program_title) + guides(colour = guide_legend(override.aes = list(size=12)))
+ggplot(schoolsall, aes(x=action_type, y=log(costmon), fill = action_type)) + geom_jitter(alpha=I(1/2), aes(color=action_type)) + facet_wrap(~cfda_program_title) + guides(colour = guide_legend(override.aes = list(size=12))) 
